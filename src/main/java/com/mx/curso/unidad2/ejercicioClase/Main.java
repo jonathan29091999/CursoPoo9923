01705de63ae41c27f18c1de1150eb3e4dca803b8
jonathan <jhony.29091810@gmail.com>
2025-11-15 08:37:59 -0600

    ejercicio 2 clase 15-nov-25

---
+++ src/main/java/com/mx/curso/unidad2/ejercicioClase/Main.java
@@ -0,0 +1,18 @@
+package com.mx.curso.unidad2.ejercicioClase;
+
+public class Main {
+    public static void main(String[] args) {
+        Mascota mascota = new Mascota();
+
+
+        System.out.println("OBJETO DE LA CLASE MASCOTA");
+        mascota.setNombreMascota("zeus");
+        mascota.setTipoMascota("perro");
+        mascota.setEdadMascota(3);
+
+        System.out.println("el tipo de mascota es : "+mascota.getTipoMascota());
+        System.out.println("El nombre de la mascota es "+mascota.getNombreMascota());
+        System.out.println("la mascota tiene "+mascota.getEdadMascota()+" años");
+
+    }
+}
---
+++ src/main/java/com/mx/curso/unidad2/ejercicioClase/Mascota.java
@@ -0,0 +1,32 @@
+package com.mx.curso.unidad2.ejercicioClase;
+
+public class Mascota
+{
+    private String tipoMascota;
+    private String nombreMascota;
+    private int edadMascota;
+
+    public String getTipoMascota() {
+        return tipoMascota;
+    }
+
+    public void setTipoMascota(String tipoMascota) {
+        this.tipoMascota = tipoMascota;
+    }
+
+    public String getNombreMascota() {
+        return nombreMascota;
+    }
+
+    public void setNombreMascota(String nombreMascota) {
+        this.nombreMascota = nombreMascota;
+    }
+
+    public int getEdadMascota() {
+        return edadMascota;
+    }
+
+    public void setEdadMascota(int edadMascota) {
+        this.edadMascota = edadMascota;
+    }
+}
